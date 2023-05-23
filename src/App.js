3ed6ee353659ca9b38339629e5a0c057eb480a31
vishu-21 <vishal211199@gmail.com>
2023-05-23 16:09:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Component/Header';
+import Sidebar from './Component/Sidebar';
+import TextEditor from './Component/TextEditor';
+// import Toolbar from './components/Toolbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex w-screen">
+      <div style={{flex: '.97'}} >
+        <div style={{top: '0.1rem', paddingBottom: '.9rem'}} className='head sticky z-10 bg-headerbg'>
+          <Header /> {/* Top most header navbar which contains the doc name and other options along with share feature */}
+         {/* <Toolbar /> toolbar just below the header navbar which contains different text formatting options  */}
+        </div>
+        <TextEditor />  {/* component in which the library Quill is used to enable the functionalities of a text editor as well as the toolbar for different text formatting options*/}
+      </div>
+      <div style={{flex: '.035'}} className='mt-2'>
+        <Sidebar /> {/* the sidebar that includes links to other Google apps */}
+      </div>
     </div>
   );
 }
